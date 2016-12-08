49588a429e53662ca697600222b9f109c09a2b51
Michael Gord <michael.l.gord@gmail.com>
2016-12-08 17:04:04 -0500

    connect sqlite db and create asset model

---
+++ examples/basic-sqlite-database.js
@@ -0,0 +1,30 @@
+var Sequelize = require('Sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect': 'sqlite',
+	'storage': 'basic-sqlite-database.sqlite'
+});
+
+var Asset = sequelize.define('asset', {
+	name: {
+		type: Sequelize.STRING
+	},
+	symbol: {
+		type: Sequelize.STRING
+	},
+	amount: {
+		type: Sequelize.STRING
+	},
+	price: {
+		type: Sequelize.STRING
+	},
+	description: {
+		type: Sequelize.STRING
+	},
+	holding: {
+		type: Sequelize.BOOLEAN
+	}
+})
+
+sequelize.sync().then(function () {
+	console.log('Everything is synced');
+});
