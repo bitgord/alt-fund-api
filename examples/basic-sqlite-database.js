3a7cbd3e77bcb1cd02d809378ecbfd852aa98de7
Michael Gord <michael.l.gord@gmail.com>
2016-12-08 17:05:31 -0500

    add dirname to db

--- examples/basic-sqlite-database.js
+++ examples/basic-sqlite-database.js
@@ -1,7 +1,7 @@
 var Sequelize = require('Sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect': 'sqlite',
-	'storage': 'basic-sqlite-database.sqlite'
+	'storage': __dirname + '/basic-sqlite-database.sqlite'
 });
 
 var Asset = sequelize.define('asset', {
